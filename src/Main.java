dd9354840e3fdb1378e68ed741903e72a076d75f
Ars_tosha <arsentev98pochta@mail.ru>
2023-05-01 11:48:41 +0500

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    private static final String REGEX = "^[a-zA-Z0-9_]*$";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            checkAuth("login","123","123");
+            System.out.println("Все хорошо");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Регистрация не пройдена");
+        }
+    }
+    private static void checkAuth(String login, String password, String confirmPassword)throws WrongLoginException,WrongPasswordException{
+        if (!login.matches(REGEX) || login.length() > 20){
+            throw new WrongLoginException("Логин неправильный");
+        }
+        if (!password.matches(REGEX) || password.length() > 20 || !password.equals(confirmPassword)){
+            throw new WrongPasswordException();
+        }
     }
 }
